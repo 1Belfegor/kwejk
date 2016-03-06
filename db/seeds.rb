e96935b67759569a8696ced267b0f9ce2d2cfb63
Slawek <slawek.kubiak@gmail.com>
2016-03-06 13:12:08 +0100

    Added rails_admin to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new( :email => "admin@example.com", :password => 'password' :password_confirmation => 'password')
+u.save
